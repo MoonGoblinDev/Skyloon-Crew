1d8e2ec77c66530774c98cc76ec80a1867354510
MoonGoblin <bregas.sat1@gmail.com>
2025-05-22 19:20:24 +0800

    Initial Commit

---
+++ Skyloon Crew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skyloon Crew
+//
+//  Created by Bregas Satria Wicaksono on 22/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skyloon Crew/Skyloon_CrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skyloon_CrewApp.swift
+//  Skyloon Crew
+//
+//  Created by Bregas Satria Wicaksono on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Skyloon_CrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
